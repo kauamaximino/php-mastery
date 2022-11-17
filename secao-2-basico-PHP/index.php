f692a75112d59c4e5bc2eee6e20ffed1564e43ae
Kauã Maximino <kaua_maximino@outlook.com>
2022-11-17 20:21:36 -0300

    :sparkles:	feat: section 2

--- secao-2-basico-PHP/index.php
+++ secao-2-basico-PHP/index.php
@@ -8,7 +8,7 @@
   if ($nome == "Kauã") {
     echo "Meu nome é $nome, e estou testando o if <br>";
   } else {
-    echo "Meu nome não é $nome, mas, mesmo assim, estou testando o if <br>";
+    echo "Meu nome não é  esse: $nome, mas, mesmo assim, estou testando o if <br>";
   }
 
 ?>
